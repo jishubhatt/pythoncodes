777291ba983c6ff37006b4dda74a8685c3626ed1
jishubhatt <121309972+jishubhatt@users.noreply.github.com>
2022-12-23 22:54:41 +0530

    Create buzzfizz2.py
    #add is divisble by 7 print bazz

---
+++ buzzfizz2.py
@@ -0,0 +1,12 @@
+if x%3==0  and  x%5==0:
+        print("fizzbuzz")
+        continue
+    elif x%3==0:
+        print("fizz")
+    elif x%5==0:
+        print("buzz")
+    elif x%7==0:
+        print("bazz")
+    
+    else:
+        print(x)    
